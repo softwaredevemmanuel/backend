bc8d5369908f8077c8fe3d41553ed699af0326a2
softwaredevemmanuel <48366297+softwaredevemmanuel@users.noreply.github.com>
2023-07-14 22:49:32 +0100

    database1

--- backend/settings.py
+++ backend/settings.py
@@ -79,11 +79,14 @@ WSGI_APPLICATION = 'backend.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+       'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'event',
+        'USER': 'postgres',
+        'PASSWORD': 'hannah',
+        'HOST': '127.0.0.1',
+        'PORT': '5432',
     }
 }
-DATABASES['default'] =dj_database_url.config()
 
 
 # Password validation
--- frontend/models.py
+++ frontend/models.py
@@ -1,3 +1,13 @@
 from django.db import models
+from django.urls import reverse
+from django.utils import timezone
+from datetime import datetime, timedelta
+import secrets
+
+
 
 # Create your models here.
+
+class Event(models.Model):
+    event_name = models.CharField(max_length=100)
+    location = models.CharField(max_length=100)
